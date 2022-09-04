d7c08fd6f4bdc4344bc6bb2e43d62cde1ab6584a
MrKisi2 <106251998+MrKisi2@users.noreply.github.com>
2022-09-04 16:24:37 -0300

    Add files via upload
    Inclusão de alteração via jquery arquivo index.html e scriptJs.js

---
+++ scriptJs.js
@@ -0,0 +1,5 @@
+var element = document.getElementById('divBase');
+element.innerHTML = '<p>Whats app (11) 9 7558 2348</p>'
+
+
+$('#divBase').html("<p>Whats app e chamadas de voz:<br>(11) 9-7558-2348</p>");
